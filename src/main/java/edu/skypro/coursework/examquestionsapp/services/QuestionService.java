143613c6aa35e89635556903983cf81f4d2d28ba
RomanKamko <r.kamko@mail.ru>
2023-05-18 13:30:39 +0400

    - описал JavaQuestionService

---
+++ src/main/java/edu/skypro/coursework/examquestionsapp/services/QuestionService.java
@@ -0,0 +1,17 @@
+package edu.skypro.coursework.examquestionsapp.services;
+
+import edu.skypro.coursework.examquestionsapp.model.Question;
+
+import java.util.Collection;
+
+public interface QuestionService {
+    Question add(String question, String answer);
+
+    Question add(Question question);
+
+    Question remove(Question question);
+
+    Collection<Question> getAll();
+
+    Question getRandomQuestion();
+}
---
+++ src/main/java/edu/skypro/coursework/examquestionsapp/services/impl/JavaQuestionService.java
@@ -0,0 +1,40 @@
+package edu.skypro.coursework.examquestionsapp.services.impl;
+
+import edu.skypro.coursework.examquestionsapp.model.Question;
+import edu.skypro.coursework.examquestionsapp.services.QuestionService;
+
+import java.util.*;
+
+public class JavaQuestionService implements QuestionService {
+    private static Set<Question> questionSet = new HashSet<>();
+    @Override
+    public Question add(String question, String answer) {
+        Question result = new Question(question, answer);
+        questionSet.add(result);
+        return result;
+    }
+
+    @Override
+    public Question add(Question question) {
+        questionSet.add(question);
+        return question;
+    }
+
+    @Override
+    public Question remove(Question question) {
+        questionSet.remove(question);
+        return question;
+    }
+
+    @Override
+    public Collection<Question> getAll() {
+        return questionSet;
+    }
+
+    @Override
+    public Question getRandomQuestion() {
+        Random random = new Random();
+        List<Question> questionList = questionSet.stream().toList();
+        return questionList.get(random.nextInt(questionList.size()));
+    }
+}
