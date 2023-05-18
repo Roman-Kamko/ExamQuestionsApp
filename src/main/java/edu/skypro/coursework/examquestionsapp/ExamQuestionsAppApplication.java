2ccbbba1cd04c3b72a95791b3330087ef76c75d9
RomanKamko <r.kamko@mail.ru>
2023-05-18 13:24:53 +0400

    initial commit

---
+++ src/main/java/edu/skypro/coursework/examquestionsapp/ExamQuestionsAppApplication.java
@@ -0,0 +1,13 @@
+package edu.skypro.coursework.examquestionsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamQuestionsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/skypro/coursework/examquestionsapp/ExamQuestionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.skypro.coursework.examquestionsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamQuestionsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
